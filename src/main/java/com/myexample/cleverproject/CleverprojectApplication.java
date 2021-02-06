4ab1cba8e22439630136af88979c4a3235de5b67
polina <polina@example.ru>
2021-02-06 16:48:57 +0300

    Initial commit

---
+++ src/main/java/com/myexample/cleverproject/CleverprojectApplication.java
@@ -0,0 +1,13 @@
+package com.myexample.cleverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/myexample/cleverproject/exceptions/NotFoundExceptions.java
@@ -0,0 +1,9 @@
+package com.myexample.cleverproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+
+public class NotFoundExceptions  extends RuntimeException  {
+}
